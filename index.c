cd332dfe005586778a79e3869eba3df1a0e40e85
Yaseen Hasan <yabuzeina02@gmail.com>
2024-11-17 21:17:10 +0200

    Modifiying on index.c file and add the last Embedded lab code

--- index.c
+++ index.c
@@ -1,7 +1,21 @@
-#include <stdio.h>
+#define GPIO2DIR (*((volatile unsigned long *) 0x50028000))
+#define GPIO2DATA (*((volatile unsigned long *) 0x50023FFC))
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
-}
+int main (void)
+ { 
+    GPIO2DIR |= 0b001111 ;
+    GPIO2DATA = 0b001001;
+   // Write your code here
+   while (1){
+      if (GPIO2DATA & 0b100000){
+	 GPIO2DATA =0b001111; 
+	 while(GPIO2DATA & 0b100000);
+      }
+      if (GPIO2DATA & 0b010000){
+	 GPIO2DATA =0b000000; 
+	 while(GPIO2DATA & 0b010000);
+      }
+   }
+   return 0;
+ }   
 
